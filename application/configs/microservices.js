2e6d6bad3433302eac1a85970c1d163311153db5
Roy Meissner <roy-meissner@gmx.net>
2016-09-29 14:56:24 +0200

    Added variable service urls by env vars

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -1,20 +1,22 @@
 'use strict';
 
+const co = require('../common');
+
 module.exports = {
   'activities': {
-    uri: 'activitiesservice.manfredfris.ch',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_ACTIVITIES)) ? process.env.SERVICE_URL_ACTIVITIES : 'activitiesservice.experimental.slidewiki.org',
     port: 80
   },
   'notification': {
-    uri: 'notificationservice.manfredfris.ch',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_NOTIFICATION)) ? process.env.SERVICE_URL_NOTIFICATION : 'notificationservice.experimental.slidewiki.org',
     port: 80
   },
   'user': {
-    uri: 'userservice.manfredfris.ch',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'userservice.experimental.slidewiki.org',
     port: 80
   },
   'deck': {
-    uri: 'deckservice.manfredfris.ch',
+    uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'deckservice.experimental.slidewiki.org',
     port: 80
   }
 };
