7350652e4ce60ae2720d5b9f0c84e4565577f687
dpaun <dpaunovic@gmail.com>
2017-02-13 15:46:40 +0100

    Removed unnecessary notification-service uri

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -7,10 +7,7 @@ module.exports = {
     uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
   'activities': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_ACTIVITIES)) ? process.env.SERVICE_URL_ACTIVITIES : 'http://activitiesservice'
-  },
-  'notification': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_NOTIFICATION)) ? process.env.SERVICE_URL_NOTIFICATION : 'http://notificationservice'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_ACTIVITIES)) ? process.env.SERVICE_URL_ACTIVITIES : 'http://activitiesservice.experimental.slidewiki.org'
   },
   'user': {
     uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'http://userservice'
