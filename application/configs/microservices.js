6861659c4eb71f547496b5397e39ab75a9e38f8e
dpaun <dpaunovic@gmail.com>
2017-02-14 08:47:38 +0100

    SWIK-994 Modified URLs to use HTTPS

--- application/configs/microservices.js
+++ application/configs/microservices.js
@@ -7,7 +7,7 @@ module.exports = {
     uri: (!co.isEmpty(process.env.SERVICE_URL_DECK)) ? process.env.SERVICE_URL_DECK : 'http://deckservice'
   },
   'activities': {
-    uri: (!co.isEmpty(process.env.SERVICE_URL_ACTIVITIES)) ? process.env.SERVICE_URL_ACTIVITIES : 'http://activitiesservice.experimental.slidewiki.org'
+    uri: (!co.isEmpty(process.env.SERVICE_URL_ACTIVITIES)) ? process.env.SERVICE_URL_ACTIVITIES : 'http://activitiesservice'
   },
   'user': {
     uri: (!co.isEmpty(process.env.SERVICE_URL_USER)) ? process.env.SERVICE_URL_USER : 'http://userservice'
